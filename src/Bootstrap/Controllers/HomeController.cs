fcaec359c1320d2754f4f7f05313ee3621758a6a
Misha <Gd.misha@gmail.com>
2016-10-10 19:09:14 +0300

    Add project files.

---
+++ src/Bootstrap/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bootstrap.Controllers {
+	[Route("[controller]/[action]")]
+	public class HomeController : Controller {
+		public IActionResult Index() {
+			return View("~/Views/Index.cshtml");
+		}
+	}
+}
